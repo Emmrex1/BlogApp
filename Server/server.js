bb322c6dfed8c5ddebbf6a4312819c1bf7117719
Emmrex1 <oyewooyewale94@gmail.com>
2025-07-27 22:36:39 +0100

    hg

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 
-// CORS configuration
+// CORS configurat
 const allowedOrigins = [
   'http://localhost:5173',
   'https://emmrexblog.vercel.app',
